8a7d0e45314c841695d55b0227ef94a75481c42e
Parker Ennis <rpennis@ncsu.edu>
2015-03-03 19:16:44 -0500

    json shows up for all three data tables

--- app/controllers/api/v1/voters_controller.rb
+++ app/controllers/api/v1/voters_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::VotersController < ApplicationController
   before_action :restrict_access
 
-  def index
-    @voter = Voter.new(voter_params)
-
-    if @voter.save
-     render json: @voter
-    else
-     render json: "Invalid parameters"
-    end
-  end
+  # def index
+  #   @voter = Voter.new(voter_params)
+  #
+  #   if @voter.save
+  #    render json: @voter
+  #   else
+  #    render json: "Invalid parameters"
+  #   end
+  # end
 
   def create
     @voter = Voter.new(voter_params)
@@ -39,12 +39,19 @@ end
 
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  private def voter_params
+  private
+
+  def voter_params
     params.require(:voter).permit(:name, :party)
   end
 
   def restrict_access
-    authenticate_or_request_with_http_token do |token, options|
-      ApiKey.exists?(access_token: token)
-    end
+   api_key = ApiKey.find_by_access_token(params[:access_token])
+   head :unauthorized unless api_key
   end
+
+  # def restrict_access
+  #   authenticate_or_request_with_http_token do |token, options|
+  #     ApiKey.exists?(access_token: token)
+  #   end
+  # end
--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::VotesController < ApplicationController
   before_action :restrict_access
 
   def index
-	  render json: Votes.all
+	  render json: Vote.all
   end
 
   def create
@@ -16,12 +16,20 @@ class Api::V1::VotesController < ApplicationController
   end
 end
   # Never trust parameters from the scary internet, only allow the white list through.
-  private def vote_params
+  private
+
+  def vote_params
     params.require(:vote).permit(:voter_id, :candidate_id)
   end
 
   def restrict_access
-    authenticate_or_request_with_http_token do |token, options|
-    ApiKey.exists?(access_token: token)
-    end
+   api_key = ApiKey.find_by_access_token(params[:access_token])
+   head :unauthorized unless api_key
   end
+
+
+  # def restrict_access
+  #   authenticate_or_request_with_http_token do |token, options|
+  #   ApiKey.exists?(access_token: token)
+  #   end
+  # end
